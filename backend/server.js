55d5dd2194cd1abf8044bdf318d8070ce1ae8bba
sakshibhardwaj24 <bhardwajsakshi678@gmail.com>
2025-04-18 17:45:23 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,7 @@ const { Server } = require("socket.io");
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 const routes = require("./routes/messageRoutes");
-<<<<<<< HEAD
 const authRoutes = require("./routes/authRoutes"); // ✅ Add this line
-=======
-const db = require("./config/db");
->>>>>>> b9b8ccdeaa8a7bcd87ecf82004bcb052cb36a920
 
 dotenv.config();
 
@@ -51,10 +47,6 @@ io.on("connection", (socket) => {
             created_at: new Date()
         };
 
-<<<<<<< HEAD
-=======
-        // Save message to MySQL database
->>>>>>> b9b8ccdeaa8a7bcd87ecf82004bcb052cb36a920
         const query = "INSERT INTO messages (sender_id, receiver_id, message, created_at) VALUES (?, ?, ?, ?)";
         db.query(query, [senderId, receiverId, message, payload.created_at], (err, result) => {
             if (err) {
@@ -62,13 +54,7 @@ io.on("connection", (socket) => {
                 return;
             }
 
-<<<<<<< HEAD
             console.log("💾 Message saved to DB");
-=======
-            console.log("Message saved to DB");
-
-            // Emit message to sender and receiver through WebSocket
->>>>>>> b9b8ccdeaa8a7bcd87ecf82004bcb052cb36a920
             io.emit(`receiveMessage:${receiverId}`, payload);
             io.emit(`receiveMessage:${senderId}`, payload);
         });
