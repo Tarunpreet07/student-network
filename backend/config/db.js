0b6254420700d1fa5018802f27a4b00ec629af51
Tarunpreet07 <kaurtarunpreet2@gmail.com>
2025-04-18 18:58:29 +0530

    working login,home and msgs

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",      // ✅ from .env
     user: process.env.DB_USER || "root",           // ✅ from .env
-    password: process.env.DB_PASSWORD || "sakshi@12345",       // ✅ from .env
+    password: process.env.DB_PASSWORD || "tarun@1",       // ✅ from .env
     database: process.env.DB_NAME || "campus_network", // ✅ from .env
     waitForConnections: true,
     connectionLimit: 10,
